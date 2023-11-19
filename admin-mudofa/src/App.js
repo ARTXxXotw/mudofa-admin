31bc6a39956688ff21556eb96df71fd29ef2a4a8
abzal <130212163+ARTXxXotw@users.noreply.github.com>
2023-11-19 19:18:50 +0200

    asds
    sdaasd

--- admin-mudofa/src/App.js
+++ admin-mudofa/src/App.js
@@ -1,22 +1,10 @@
 
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./pages/Home";
 import Product from "./pages/Product";
-import Spetsalist from "./pages/Spetsalist";
-import Filyal_mark from "./pages/Filyal_mark";
-import Metro from "./pages/Metro";
-import Rayon from "./pages/Rayon";
-import Zakaz from "./pages/Zakaz";
-import Xususiyat from "./pages/Xususiyat";
-import Tables from "./pages/Tables";
-import Mutahasis_time from "./pages/Mutahasis_time";
-import Billing from "./pages/Billing";
-import Rtl from "./pages/Rtl";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
+
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
-import User from "./pages/User"
+
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
@@ -33,18 +21,7 @@ function App() {
           </>):(
         <Main>
         <Route exact path="/Product" component={Product} />
-        <Route exact path="/User" component={User} />
-        <Route exact path="/Filyal_mark" component={Filyal_mark} />
-        <Route exact path="/Metro" component={Metro} />
-        <Route exact path="/Rayon" component={Rayon} />
-        <Route exact path="/Xususiyat" component={Xususiyat} />
-        <Route exact path="/Zakaz" component={Zakaz} />
-        <Route exact path="/tables" component={Tables} />
-        <Route exact path="/Spetsalist" component={Spetsalist} />
-        <Route exact path="/Mutahasis_time" component={Mutahasis_time} />
-        <Route exact path="/billing" component={Billing} />
-        <Route exact path="/rtl" component={Rtl} />
-        <Route exact path="/profile" component={Profile} />
+
         <Redirect from="/Product" to="/Product" />
         </Main>
         )}
--- admin-mudofa/src/pages/Product.js
+++ admin-mudofa/src/pages/Product.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 export default function Product() {
   const [data,setData]=useState([]);
   const [dataiD,setDataiD]=useState([]);
+  const [data1iD,setData1iD]=useState([]);
   useEffect(()=>{
     axios.get(`https://new-uzbek.onrender.com/api/v1/new_action/`).then(res=>{
       setData(res.data)
@@ -16,8 +17,7 @@ export default function Product() {
     var data = new FormData;
     data.append(`image`, document.querySelector("#bir").value)
     data.append(`desc`, document.querySelector("#ikki").value)
-    data.append(`time_create`,"")
-    data.append(`time_update`, "")
+    data.append(`news_id`,1)
   
        axios.put(`https://new-uzbek.onrender.com/api/v1/new_action/${dataiD}`,data,{headers:{Authorization:`Bearer ${sessionStorage.getItem("token")}`}}).then(res=>{
       alert("Успешно")
@@ -30,8 +30,34 @@ export default function Product() {
     setDataiD(id)
     document.querySelector(".bu-filyal-omagadasdessd").style=`display:block`
   }
+  function postmetod(){
+    var data = new FormData;
+    data.append(`image`, document.querySelector("#post1").value)
+    data.append(`desc`, document.querySelector("#post2").value)
+    data.append(`news_id`, document.querySelector("#post3").value)
+  
+       axios.post(`https://new-uzbek.onrender.com/api/v1/new_action/`,data,{headers:{Authorization:`Bearer ${sessionStorage.getItem("token")}`}}).then(res=>{
+      alert("Успешно")
+      window.location.reload()
+    }).catch(err=>{
+      alert("error")
+    })
+  }
+  function deltemetodId(id){
+    setData1iD(id)
+    document.querySelector(".bu-filyala-assdde").style=`display:block`
+  }
+  function detelmetod(){
+    axios.delete(`https://new-uzbek.onrender.com/api/v1/new_action/${data1iD}`,{headers:{Authorization:`Bearer ${sessionStorage.getItem("token")}`}}).then(res=>{
+      alert("Вы удалили ")
+      window.location.reload()
+    })
+  }
   return (
     <div>
+      <div className="all-btn">
+        <button onClick={()=>document.querySelector(".bu-filyal-omagadasdessd1").style=`display:block`}>dabavit</button>
+      </div>
       
       <table id="customers">
   <tr>
@@ -48,10 +74,10 @@ export default function Product() {
         <>
         <tr>
         <td>{item.id}</td>
-        <td><img src={item.image} alt="" /></td>
+        <td><img className='table-img' src={item.image} alt="" /></td>
         <td>{item.desc}</td>
-        <td>{item.time_create}</td>
-        <td><button>delete</button></td>
+        <td>{item.time_create.slice(0,10)}</td>
+        <td><button onClick={()=>deltemetodId(item.id)}>delete</button></td>
         <td><button onClick={()=>editmalumot(item.id)}>edit</button></td>
        </tr>
         </>
@@ -79,6 +105,40 @@ export default function Product() {
     </div>
 </div>
 </div>
+
+<div className="bu-filyal-omagadasdessd1" style={{display:"none"}}>
+
+<div className="modal-delete">
+    <div className="modal-ichi-inp">
+      <div className="x-dv">
+<div className="xx"  onClick={()=>document.querySelector(".bu-filyal-omagadasdessd1").style=`display:none`}>
+  X 
+</div>
+</div>
+        <span>image</span><br />
+        <input type="text"  id='post1' /><br />
+        <span>desc</span><br />
+        <input type="text" id='post2' /><br />
+        <span>news_id</span><br />
+        <input type="number" id='post3'/><br />
+        <button onClick={()=>postmetod()} >dabavit</button>
+    </div>
+</div>
+</div>
+
+<div className="bu-filyala-assdde">
+<div className="modal-delete">
+    <div className="modal-ichi">
+      <p>Вы действительно хотите удалить этот</p>
+      <div className="btn-modal">
+      <button onClick={()=>detelmetod()}>ДА</button>
+      <button onClick={()=>document.querySelector(".bu-filyala-assdde").style=`display:none`}>Нет</button>
+      
+      </div>
+    </div>
+</div>
+</div>
+
 
     </div>
   )
